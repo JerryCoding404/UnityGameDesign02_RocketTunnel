c66b2598e30a5976dd1f8ff6ed7804a5bd49ec07
JerryCoding <62367404+JerryCoding404@users.noreply.github.com>
2020-06-18 15:46:29 +0800

    Init State

---
+++ Assets/Rocket.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+        Thrust();
+        Rotate();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK");
+                break;
+            case "Fuel":
+                print("Fuel");
+                break;
+            default:
+                print("Dead");
+                break;
+        }
+    }
+    void Thrust()
+    {
+        float thrustThisFrame = mainThrust * Time.deltaTime;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
+            if (audioSource.isPlaying == false)
+            {
+                audioSource.Play();
+            }
+            // print("Thrusting!!");
+        }  
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    void Rotate()
+    {
+        rigidBody.freezeRotation = true; //控制物件旋轉後不會亂動。
+        float rotateThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotateThisFrame);
+            // print("Rotate Left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotateThisFrame);
+            // print("Rotate Right");
+        }
+
+        rigidBody.freezeRotation = false; //在按完按鍵後將物體的旋轉關掉，不能控制旋轉，回到原本的狀態。
+    }
+}
